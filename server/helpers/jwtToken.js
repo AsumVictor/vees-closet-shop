c49b81de9be3b7e2800598d3bdc1d948ed57bfaa
AsumVictor <iamasum369@gmail.com>
2023-07-22 13:43:38 -0700

    Added cross access

--- server/helpers/jwtToken.js
+++ server/helpers/jwtToken.js
@@ -4,6 +4,8 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: true,
+    sameSite: "Lax"
   };
 
   res.status(statusCode).cookie("token", token, options).json({
