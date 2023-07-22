f7ac5e2c937329b5f2c62a9c657c675e214fe996
AsumVictor <iamasum369@gmail.com>
2023-07-22 13:55:51 -0700

    Fix file destination error

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const catchAsyncErrors = require("../middleware/CatchAsyncErrors");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
