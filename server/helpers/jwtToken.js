f49fb405f1c8420cbd766cb4f8c0501c1474eecf
AsumVictor <iamasum369@gmail.com>
2023-07-23 00:54:38 -0700

    Fix cookies

--- server/helpers/jwtToken.js
+++ server/helpers/jwtToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "Lax",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
