6cc095b0ec7d3057678249abb49c7d0c4a4ecedc
AsumVictor <Iamasum369@gmail.com>
2023-09-11 15:26:02 +0000

    Deploy v9 s

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://veescloset.onrender.com",
     credentials: true,
   })
 );
